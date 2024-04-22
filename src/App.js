055ecf2a4fe85aeb6d19840f0ace88f81bb3c8da
Syed Abdullah Shah <95468603+abdullah117765@users.noreply.github.com>
2024-04-23 00:26:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import React from 'react';
 import {
-
-  BrowserRouter, Routes, Route
+  BrowserRouter, 
+  Routes, 
+  Route
 } from 'react-router-dom';
 
 import './App.css';
 import { CheckList } from './components/testing';
 
+function AwsDeploymentMessage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <p>This React project is deployed on AWS.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CheckList />} />
-      </Routes>
-    </BrowserRouter>      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CheckList />} />
+        </Routes>
+      </BrowserRouter> 
+      <AwsDeploymentMessage />     
     </>
   );
 }
